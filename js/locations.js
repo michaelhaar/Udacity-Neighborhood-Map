90ac8860239762d804123cf6d5736ef8b1ae8cba
Michael Haar <michael.haar@gmx.at>
2017-11-04 13:50:53 +0100

    Added some locations

--- js/locations.js
+++ js/locations.js
@@ -6,4 +6,32 @@ var locations = [
       lng: 16.008057
     }
   }
+  {
+    title: 'Paragleiten Schöckl',
+    location: {
+      lat: 47.201044,
+      lng: 15.4740111
+    }
+  }
+  {
+    title: 'Wandern Bärenschützklamm',
+    location: {
+      lat: 47.336009,
+      lng: 15.375015
+    }
+  }
+  {
+    title: 'Schöckl Westgipfel Wanderung',
+    location: {
+      lat: 47.1981282,
+      lng: 15.4599248
+    }
+  }
+  {
+    title: 'Raabklamm Wanderung',
+    location: {
+      lat: 47.2166667,
+      lng: 15.5666667
+    }
+  }
 ]
